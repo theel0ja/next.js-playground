3f1cbc9d28ee24c0fef0e3d4e63ff1a9717a0617
Elias Ojala <git@eliasojala.me>
2018-02-10 18:53:43 +0200

    build scripts, etc.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const next = require('next')
 
+const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -21,7 +22,7 @@ app.prepare()
 
   server.listen(3000, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${port}`)
   })
 })
 .catch((ex) => {
